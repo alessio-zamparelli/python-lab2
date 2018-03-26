dedc242ee871ea939cdb0c7f1e28d1b6809ee216
alessio-zamparelli-polito <alessio.zamparelli@studenti.polito.it>
2018-03-26 15:57:44 +0200

    first

--- main1.py
+++ main1.py
@@ -1,18 +1,22 @@
 tasks_list = list()
 
-def todo_manager:
+def todo_manager():
     while(1):
-        ch=int(input())
-        if(ch==1):
-            insert_task()
-        elif (ch==2):
-            remove_task()
-        elif (ch==3):
-            show_tasks()
-        elif (ch==4):
-            exit(0)
-        else:
-            print("error in input command! retry again")
+        try:
+            print("cosa vuoi fare?\n1 inserisci\n2 remove\n3 show\n4 exit")
+            ch=int(input())
+            if(ch==1):
+                insert_task()
+            elif (ch==2):
+                remove_task()
+            elif (ch==3):
+                show_tasks()
+            elif (ch==4):
+                exit(0)
+            else:
+                print("error in input command! retry again")
+        except ValueError:
+            print("Valore inserito non riconosciuto, le possibilità sono 1,2,3,4")
 
 
 def insert_task():
@@ -23,9 +27,22 @@ def insert_task():
 def remove_task():
     print("Write the item to delete")
     to_be_deleted=input()
-    try
+    try:
         tasks_list.remove(to_be_deleted)
-        catch
+    except ValueError:
+        print("element not found!")
 def show_tasks():
+    if len(tasks_list) == 0:
+        print("no tasks memorized yet")
+        return
+    tasks_list.sort()
+    print(*tasks_list, sep='\n')
 
 
+
+def main():
+    todo_manager()
+    return 0
+
+if __name__ == "__main__":
+    main()
